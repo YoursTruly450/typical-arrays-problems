25d3a0462467fb65dba8dd41ae28bedbb01c94b5
YoursTruly450 <yourstruly.bill@gmail.com>
2021-03-11 15:21:06 +0400

    feat: fix functions min(), max(), average()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array) {
+    if (array.length === 0) {
+      return 0;
+    } else {
+      return Math.min (...array);
+    }
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array) {
+    if (array.length === 0) {
+      return 0;
+    } else {
+      return Math.max(...array)
+    }
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array) {
+    if (array.length === 0) {
+      return 0;
+    } else {
+      return array.reduce((a,b) => a + b, 0) / array.length;
+    }
+  } else {
+    return 0;
+  }
 }
